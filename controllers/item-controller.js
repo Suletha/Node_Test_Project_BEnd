def7630142946d14b1bb236ce17ead961d7559d6
suletha <suletha.gopi@gmail.com>
2023-11-28 15:18:35 +0530

    db_updated

--- controllers/item-controller.js
+++ controllers/item-controller.js
@@ -1,57 +1,56 @@
-const { it } = require('node:test');
-const Item = require('../models/item');
-exports.getItems = async(req,res,next) => {
-    try{
-        const result = await Item.findAll();
-        res.json(result);
-    }catch(err){
-        console.error(err);
-        res.status(500).json({error:'some Error in fetching items'})
+const Item=require('../models/item');
 
+exports.getItems= async(req,res,next)=>{
+    try{
+      const result = await Item.findAll();
+      res.json(result);
+    }catch (err){
+      console.error(err);
+      res.status(500).json({ error: 'An error occurred while fetching items.' });
     }
 };
-
-
-exports.postItem = async(req,res,next) => {
+  
+exports.postItem= async(req,res,next)=>{
     const { name, desc, price, qty } = req.body;
-    try{
+    try {
         const newItem = await Item.create({
-            name: name,
-            desc: desc,
-            price: price,
-            qty: qty
-
-        })
-        console.log(newItem.id)
+        name: name,
+        desc: desc,
+        price: price,
+        qty: qty
+        });
+        console.log(newItem.id);
         res.status(201).json(newItem);
-    }catch(err){
+    } catch (err) {
         console.error(err);
-        res.status(500).json({error:'some Error in creating items'})
-
+        res.status(500).json({ error: 'An error occurred while creating the item.' });
     }
-}
+};
+  
 
-exports.updateItem = async(req,res,next) => {
-    const itemId = req.params.id;
+exports.updateItem= async(req,res,next)=>{
+    const itemId=req.params.id;
     try{
-        const item = await Item.findByPk(itemId);
+        const item=await Item.findByPk(itemId);
         if(!item){
-            return res.status(404).json({ error:'item unavailable'});
-
+            return res.status(404).json({ error: 'Item not found' });
         }
-        //To Update the Quantity
+        //update quantity
         let qty = req.body.qty;
-        if(qty == 0){
+        // delete the item if quantity is 0
+        if(qty===0){
             await item.destroy();
-            res.status(200).json({error:'Item deleted'});
-        }else{
-            item.qty = qty;
+            // Save the updated item
+            res.status(200).json({error:'item is deleted successfully'});
+        }
+        else{
+            item.qty=qty;
             await item.save();
             res.json(item);
         }
     }
     catch(err){
         console.error(err);
-        res.status(500).json({error: 'Error in updating items'})
+        res.status(500).json({ error: 'An error occurred while updating the item' });
     }
-}
+};
