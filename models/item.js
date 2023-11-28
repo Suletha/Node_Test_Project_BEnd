9e2f93d3019dc34a91900490dba1bdc79727f3ac
suletha <suletha.gopi@gmail.com>
2023-11-28 12:51:21 +0530

    table_created

--- models/item.js
+++ models/item.js
@@ -1,25 +1,27 @@
-const Sequelize = require('sequalize');
-const sequalize = require('../util/database');
-const User = sequalize.define('item',{
-    id:{
-        type:Sequelize.INTEGER,
-        autoIncrement:true,
-        allowNull:false,
-        primaryKey:true
-      },
-      name:Sequelize.STRING,
-      desc:{
-        type:Sequelize.STRING,
-        allowNull:false
-      },
-      price:{
-        type:Sequelize.INTEGER,
-        allowNull:false
-      },
-      qty:{
-        type:Sequelize.INTEGER,
-        allowNull:false
-      }
+const Sequelize=require('sequelize');
 
+const sequelize=require('../util/database');
+
+const User=sequelize.define('item',{
+  id:{
+    type:Sequelize.INTEGER,
+    autoIncrement:true,
+    allowNull:false,
+    primaryKey:true
+  },
+  name:Sequelize.STRING,
+  desc:{
+    type:Sequelize.STRING,
+    allowNull:false
+  },
+  price:{
+    type:Sequelize.INTEGER,
+    allowNull:false
+  },
+  qty:{
+    type:Sequelize.INTEGER,
+    allowNull:false
+  }
 })
-module.exports = User;
+
+module.exports=User;
